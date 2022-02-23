2e3992bbc30007b10ae6ca4a97e528e7cebcf531
root <root@lib-p-lx1.stlrg.gv.at>
2022-02-23 10:03:34 +0100

    create onleihe user via ftp upload

--- bin/create_onleihe_user_via_upload.pl
+++ bin/create_onleihe_user_via_upload.pl
@@ -2,17 +2,65 @@ use Koha::Plugin::HKS3Onleihe::MungeRecord4Onleihe::MungeRecord4OnleiheControlle
 
 use Data::Dumper;
 use C4::Context;
+use Text::CSV;
+use Net::SFTP::Foreign;
+use Modern::Perl;
+use Date::Calc qw( Today );
+
 my $plugin = new Koha::Plugin::HKS3Onleihe::MungeRecord4Onleihe;
 my $branchcode = $plugin->retrieve_data('Branchcode');
 
-my $sql = 'select borrowernumber, cardnumber, dateofbirth, debarred, debarredcomment, dateenrolled, dateexpiry, lost, updated_on, anonymized, lastseen from borrowers';
+die "not host data" unless $ENV{ONLEIHE_HOST};
+
+my $date;
+my ($y, $m, $d) = Date::Calc::Today();
+
+my $today = sprintf('%04d-%02d-%02d', $y, $m, $d);
+
+$date = $ARGV[0] ? $ARGV[0] : $today;
+
+my $sql = <<'SQL';
+with cte_users as ( 
+	select borrowernumber, cardnumber, date_format(dateofbirth, "%d.%m.%Y") dob, 0 fsk, 0 status, "D" crud, "STMK" bib, dateexpiry m_date from borrowers 
+	union 
+	select borrowernumber, cardnumber, date_format(dateofbirth, "%d.%m.%Y"), 0, 3, "I", "STMK", dateenrolled from borrowers where categorycode <> 'OA'
+	union
+	select borrowernumber, cardnumber, date_format(dateofbirth, "%d.%m.%Y"), 0, 1, "I", "STMK", dateenrolled from borrowers where categorycode = 'OA'
+	union
+	select borrowernumber, cardnumber, date_format(dateofbirth, "%d.%m.%Y"), 0, 3, "U", "STMK", cast(updated_on as date) from borrowers where categorycode <> 'OA'  
+        -- select borrowernumber, cardnumber, date_format(dateofbirth, "%d.%m.%Y"), 0, if(debarred > '2022-02-21', 1, 3), "U", "STMK", cast(updated_on as date) from borrowers where categorycode <> 'OA'
+	)
+select  borrowernumber, cardnumber, dob, fsk, status, crud, bib  from cte_users where m_date = ?
+SQL
+
+my $dir = $ENV{ONLEIHE_USER_DIR};
+my $datename = $date;
+$datename =~ s/-//g;
+my $filename = sprintf("%s/%s_stmk.csv", $dir, $datename);
+print("$filename\n");
+
 my $dbh = C4::Context->dbh;
 my $query = $dbh->prepare($sql);
-$query->execute();
-my $borrowers = $query->fetchall_arrayref({});
+$query->execute($date);
+my $borrowers = $query->fetchall_arrayref([]);
 
 # print Dumper $borrowers;
+my $csv = Text::CSV->new ({ sep_char => ';', auto_diag => 1 });
+open my $fh, ">:encoding(utf8)", $filename;
+ # $csv->say ($fh, $_) for @rows;
+
 foreach my $borrower (@$borrowers) {
-    print Dumper $borrower;
+	#  $csv->combine(@$borrower);
+    $csv->say($fh, $borrower);
 }
+
+my $host = {
+    host => $ENV{ONLEIHE_HOST},
+    user => $ENV{ONLEIHE_USER},
+    password => $ENV{ONLEIHE_PASSWORD},
+};
+
+my $sftp = Net::SFTP::Foreign->new(%$host);
+
+$sftp->put($filename);
 
